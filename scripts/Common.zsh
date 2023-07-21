97325219cd0b2c4d2691a87568b786b35463c43a
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2023-07-21 20:23:49 +0900

    sort features

--- scripts/Common.zsh
+++ scripts/Common.zsh
@@ -1,63 +1,58 @@
-# Japanese setting
+# Japanese settings
 export LC_ALL=ja_JP.UTF-8
 export LANG=ja_JP.UTF-8
 
-# Use colors
-autoload -Uz colors
-colors
-
+# Complement settings
 # Use compinit
 autoload -Uz compinit
 compinit
+# Auto correct commands
+setopt correct
+# Hit correct large character
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# Chose correct commands by arrow key
+zstyle ':completion:*:default' menu select=2
 
-# Share history
-setopt share_history
-setopt histignorealldups
-HISTFILE=$ZDOTDIR/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-
-# Auto print date in history
-alias history='fc -lt '%F %T' 1'
-
+# Directory switch settings
 # Omit cd command
 setopt auto_cd
-
 # Auto pushd
 setopt auto_pushd
 setopt pushd_ignore_dups
 
-# Auto correct commands
-setopt correct
+# History settings
+# Share history
+HISTFILE=$ZDOTDIR/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt share_history
+setopt histignorealldups
+# Auto print date in history
+alias history='fc -lt '%F %T' 1'
 
-# Chose correct commands by arrow key
-zstyle ':completion:*:default' menu select=2
-
-# Hit correct large character
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# Set key bind
-xmodmap ~/.Xmodmap/.Xmodmap
-
-# Set shell looks
-source "$(dirname "$0")/CreatePrompt.zsh"
+# Shell looks settings
+source "$ZDOTDIR/scripts/CreatePrompt.zsh"
 PROMPT="$(prompt)"
 unsets
 unset -f unsets
 
+# Set key bind
+xmodmap ~/.Xmodmap/.Xmodmap
+
+# Functions
 # Run mkdir and cd at the same time
 function mkcd() {
-  if [[ -d $1 ]]; then
+  if [[ -d "$1" ]]; then
     echo "$1 already exists"
-    cd $1 || exit
+    cd "$1" || exit
   else
-    mkdir -p $1 && cd $1 || exit
+    mkdir -p "$1" && cd "$1" || exit
   fi
 }
 
-# Set alias
+# Aliases
 alias exz='exec zsh'
-alias ls='ls --color=auto'
 alias \$=''
 alias \#='sudo'
 alias rma='rm -r'
+alias ls='ls --color=auto'
