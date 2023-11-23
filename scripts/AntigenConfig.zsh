a4ba762222f63d722213dff50a6d4998e087d9ec
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2023-11-24 06:04:56 +0900

    quiet antigen

--- scripts/AntigenConfig.zsh
+++ scripts/AntigenConfig.zsh
@@ -2,6 +2,6 @@
 source "$ZDOTDIR/modules/antigen/antigen.zsh"
 
 # Read packages
-antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-autosuggestions 1>/dev/null 2>&1
 
 antigen apply
