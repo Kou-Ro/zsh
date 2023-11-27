5daf955068cef92c7d09a52e7389a303c4e90324
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2023-11-27 15:49:14 +0900

    remove git submodule

--- scripts/ZshConfig.zsh
+++ scripts/ZshConfig.zsh
@@ -10,8 +10,26 @@ zcon-update() {
   stashListA="$(git -C "$ZDOTDIR" stash list 2>/dev/null )"
   git -C "$ZDOTDIR" checkout main
   git -C "$ZDOTDIR" pull
+  compdir="${ZDOTDIR}/completion"
+  mv "${compdir}/git-completion.bash" "${compdir}/git-completion.bash.old" && \
+  wget -o "${compdir}/git-completion.bash" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+  if [[ ${?} -ne 0 ]]; then
+    rm -f "${compdir}/git-completion.bash.old"
+  else
+    rm -f "${compdir}/git-completion.bash"
+    mv "${compdir}/git-completion.bash.old" "${compdir}/git-completion.bash"
+  fi
+  mv "${compdir}/_git" "${compdir}/_git.old" && \
+  wget -o "${compdir}/_git" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh"
+  if [[ ${?} -ne 0 ]]; then
+    rm -f "${compdir}/_git.old"
+  else
+    rm -f "${compdir}/_git"
+    mv "${compdir}/_git.old" "${compdir}/_git"
+  fi
+  
   if [[ "$stashListA" != "$stashListB" ]];then
     git -C "$ZDOTDIR" stash pop stash@\{0\}
   fi
   exz
-}
+  }
