63113892c3a76e07ff85fa2e6936c9262c586fa3
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2023-12-01 02:01:30 +0900

    install script

---
+++ install.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Create .zshenv symbolic link in Home
+ln -s --backup=simple -S .bak ~/.dotfiles/zsh/.zshenv ~/
+
+# Skip worktree
+git update-index --skip-worktree .zshenv
+git update-index --skip-worktree .zshrc
+git update-index --skip-worktree .profile
+git update-index --skip-worktree .zlogin
+git update-index --skip-worktree .zlogout
