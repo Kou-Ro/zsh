15b994840e59d7c53fb1dbf12d2d75dee3ada349
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2023-12-01 02:07:44 +0900

    Fix cahr

--- install.zsh
+++ install.zsh
@@ -6,6 +6,6 @@ ln -s --backup=simple -S .bak ~/.dotfiles/zsh/.zshenv ~/
 # Skip worktree
 git update-index --skip-worktree .zshenv
 git update-index --skip-worktree .zshrc
-git update-index --skip-worktree .profile
+git update-index --skip-worktree .zprofile
 git update-index --skip-worktree .zlogin
 git update-index --skip-worktree .zlogout
