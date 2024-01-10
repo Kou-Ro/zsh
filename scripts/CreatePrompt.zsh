8633d3124956e73ab0e49d241ddbf53de9bcc958
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2024-01-10 17:19:16 +0900

    Chenge plompt color

--- scripts/CreatePrompt.zsh
+++ scripts/CreatePrompt.zsh
@@ -1,6 +1,6 @@
 # Read color config
 readColor() (
-  DEFCOLORS=(7 93)
+  DEFCOLORS=(248 93)
   COLORPATH="$ZDOTDIR/config/color"
   if [[ -e "$COLORPATH" ]];then
     source "$COLORPATH" > /dev/null
