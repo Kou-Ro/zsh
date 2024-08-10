bd3e2fd6aac6daf2e3c9e86a4ea14c0862147906
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2024-08-11 07:48:40 +0900

    fixZinitNotfound

--- scripts/ZinitConfig.zsh
+++ scripts/ZinitConfig.zsh
@@ -1,7 +1,8 @@
 # Setup zinit
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME" && source "${ZINIT_HOME}/zinit.zsh"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME" && source "$ZINIT_HOME/zinit.zsh"
+source "${ZINIT_HOME}/zinit.zsh"
 
 # Load plugins
 zinit load zsh-users/zsh-autosuggestions
